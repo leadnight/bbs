d541124b9e2366d41e7e2e8cbe9d334860f8d012
日色 史晃 <n-hiiro@fournetsha.local>
2016-08-08 17:09:00 +0900

    2016/08/08 17:08

---
+++ application/models/Login.php
@@ -0,0 +1,6 @@
+<?php
+class Login extends CI_Model {
+
+
+
+}
---
+++ application/views/templates_c/da7c0da15e1f1bb8543a8482af621043bf1307ab_0.file.login.html.php
@@ -0,0 +1,34 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-08-08 16:13:41
+  from "/mnt/hgfs/workspace/html/application/views/templates/login.html" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_57a8312581f0e0_62709314',
+  'file_dependency' => 
+  array (
+    'da7c0da15e1f1bb8543a8482af621043bf1307ab' => 
+    array (
+      0 => '/mnt/hgfs/workspace/html/application/views/templates/login.html',
+      1 => 1470639827,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_57a8312581f0e0_62709314 ($_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>仮ログイン画面</h1>
+</body>
+</html><?php }
+}
